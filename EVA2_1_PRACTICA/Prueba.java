ec0dc6f40fee56c6a2e255c5615b13d7034a37f8
24550009 <159494911+24550009@users.noreply.github.com>
2024-03-05 16:14:31 -0600

    Add files via upload

---
+++ EVA2_1_PRACTICA/Prueba.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Prueba{ 
+              public static void main(String args[]){
+                           Scanner captura = new Scanner(System.in);
+                          System.out.println("introduce tu nobre:");
+                           String nombre=captura.nextLine();
+                           System.out.println(nombre);
+                    }
+}
