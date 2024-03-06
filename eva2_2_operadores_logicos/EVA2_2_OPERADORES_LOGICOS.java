75b40f79f23a949ab664103a8542dec8c3f71c4f
24550009 <159494911+24550009@users.noreply.github.com>
2024-03-06 16:53:57 -0600

    Add files via upload

---
+++ eva2_2_operadores_logicos/EVA2_2_OPERADORES_LOGICOS.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_2_operadores_logicos;
+
+/**
+ *
+ * @author artas
+ */
+public class EVA2_2_OPERADORES_LOGICOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // 
+        boolean A,B;
+        A= false;
+        B= false;
+        boolean resu;
+        //operador and -->&&
+        resu = A && B;
+        System.out.println("Resu= " +resu);
+        A=true;
+        B=false;
+        resu = A && B; 
+        System.out.println("Resu= " +resu);
+        A=true;
+        B=true;
+        resu = A && B;
+        System.out.println("Resu= "+resu);
+        //operador or -->||
+        A= false;
+        B= false;
+        resu= A||B;
+        System.out.println("Resu (or)="+resu);
+        A= true;
+        B= false;
+        resu= A||B;
+        System.out.println("Resu (or)="+resu);
+        //NEGACION ---!
+        A = false;
+        resu =!A;
+        System.out.println("Resu (!)="+resu);
+        
+         
+        
+        
+        
+               
+    }
+    
+}
